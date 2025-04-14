a832de94938d1548af396d1024b7dd517a50b765
Shubham-g143 <shubhamgorwade3012@gmail.com>
2025-04-14 12:10:24 +0000

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,24 @@
+"use stra=ict";
+
+//alert(3+3)  // we are using js not a browser engine
+console.log(3+3)
+console.log("Shubham")
+
+let name = "shubham"
+let age = 24
+letisLogIn = false
+
+// number => range 2 to power of 53
+// BigInt
+// String => '' or ""
+// Boolean => true/false
+// null => stand alone value
+// undefined => the value is not assigned yet
+// Symbol => unique
+
+// Object
+
+console.log(typeof "shubham")
+console.log(typeof age)
+console.log(typeof null)
+console.log(typeof undefined)
